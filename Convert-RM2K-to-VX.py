acf1e01c8933536ec19fb086f197a579787a3f2f
Feldherren <rpaliwoda@googlemail.com>
2014-12-24 18:57:29 +0000

    Finished tool; now converts 2000/2k3 format autotiles to something roughly approaching VX (Ace) format. Depending on the tiles, though, may not come out of it looking that nice.

--- Convert-RM2K-to-VX.py
+++ Convert-RM2K-to-VX.py
@@ -13,32 +13,48 @@ def convert(infile):
 	p = os.path.dirname(os.path.abspath(__file__))
 	if not os.path.exists(p + "\\Converted"): os.makedirs(p + "\\Converted")
 	im = Image.open(infile)
-	converted = Image.new('RGB',(64,96))
+	converted = Image.new('RGB',(32,48))
 	
 	# full tile
-	converted.paste(im.crop((0,0,288,32)), (0,96,288,128))
+	#converted.paste(im.crop((16,32,32,48)), (8,24,24,40))
+	# upper left to lower right
+	converted.paste(im.crop((16,32,24,40)), (16,32,24,40))
+	# lower right to upper left
+	converted.paste(im.crop((24,40,32,48)), (8,24,16,32))
+	# upper right to lower left
+	converted.paste(im.crop((24,32,32,40)), (8,32,16,40))
+	# lower left to upper right
+	converted.paste(im.crop((16,40,24,48)), (16,24,24,32))
 	
-	# move up-facing poses to the correct position
-	converted.paste(im.crop((0,0,288,32)), (0,96,288,128))
-	converted.paste(im.crop((0,128,288,160)), (0,224,288,256))
+	# inside corners
+	converted.paste(im.crop((32,0,48,16)), (16,0,32,16))
 	
-	# move right-facing poses to the correct position
-	converted.paste(im.crop((0,32,288,64)), (0,64,288,96))
-	converted.paste(im.crop((0,160,288,192)), (0,192,288,224))
+	# outside corners
+	# upper left
+	converted.paste(im.crop((0,16,8,24)), (0,16,8,24))
+	# lower left
+	converted.paste(im.crop((0,56,8,64)), (0,40,8,48))
+	# upper right
+	converted.paste(im.crop((40,16,48,24)), (24,16,32,24))
+	# lower right
+	converted.paste(im.crop((40,56,48,64)), (24,40,32,48))
 	
-	# move down-facing poses to the correct position
-	converted.paste(im.crop((0,64,288,96)), (0,0,288,32))
-	converted.paste(im.crop((0,192,288,224)), (0,128,288,160))
+	# edges
+	# up
+	converted.paste(im.crop((16,16,32,24)), (8,16,24,24))
+	# right
+	converted.paste(im.crop((40,32,48,48)), (24,24,32,40))
+	# down
+	converted.paste(im.crop((16,56,32,64)), (8,40,24,48))
+	# left
+	converted.paste(im.crop((0,32,8,48)), (0,24,8,40))
 	
-	# move left-facing poses to the correct position
-	converted.paste(im.crop((0,96,288,128)), (0,32,288,64))
-	converted.paste(im.crop((0,224,288,256)), (0,160,288,192))
-	
-	# # Resize converted image
-	# converted = converted.resize((576,512))
+	# single tile
+	converted.paste(im.crop((16,0,32,16)), (0,0,16,16))
 	
 	# Save converted image in PNG format
 	converted.resize((64,96)).save("Converted\\[VX] " + file + ".png", 'PNG')
+	#converted.save("Converted\\[VX] " + file + ".png", 'PNG')
 
 for infile in glob.glob("*.bmp"):
 	convert(infile)
